e4f00bca3fb7ef675a2619ee837e8a4375c8ce38
Lotheraz <sult_oz@yahoo.com>
2024-10-16 14:50:54 +0200

    Add project files.

---
+++ MiningCalc/Calculator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiningCalc
+{
+    public class Calculator
+    {
+        public enum RockTypesEnum
+        {
+            Quantainium = 1,
+            Bexalite = 2,
+            Taranite = 3,
+            Borase = 4,
+            Laranite = 5,
+            Hepaestanite = 6,
+            Aluminium = 7,
+        }
+
+        private static readonly Dictionary<RockTypesEnum, double> rockValues = new Dictionary<RockTypesEnum, double>() 
+        { 
+            { RockTypesEnum.Quantainium, 176.18 },
+            { RockTypesEnum.Bexalite, 80.960 },
+            { RockTypesEnum.Taranite, 64.9 },
+            { RockTypesEnum.Borase, 64.88 },
+            { RockTypesEnum.Laranite, 60.5 },
+            { RockTypesEnum.Hepaestanite, 29.280 },
+            { RockTypesEnum.Aluminium, 1.4 },
+        };
+
+        public static double GetRockScuValue(RockTypesEnum rockType)
+        {
+            return rockValues[rockType];
+        }
+
+        public static double CalculateAuec(RockTypesEnum rockType, double mass, double purity)
+        {
+            if (purity <= 0 || mass <= 0) return 0;
+
+            double rockValue = rockValues[rockType];
+            return (mass * rockValue) * (purity / 100);
+        }
+
+        public static double CalculateScu(double mass, double purity)
+        {
+            if (purity <= 0 || mass <= 0) return 0;
+            
+            return (mass * 0.02) * (purity / 100);
+        }
+
+        public void example()
+        {
+            double QuantainiumRockValue = rockValues[RockTypesEnum.Quantainium];
+
+        }
+}
+}
---
+++ MiningCalc/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Text;
+using static MiningCalc.Calculator;
+
+namespace MiningCalc
+{
+    internal class Program
+    {
+        static string seperator = "------------------------------------------------------------------------------------------------------------------------";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome to the Quantanium mining calculator");
+            Console.WriteLine("This will give you an estimated SCU and aUEC value of based on your rock type, purity and mass input");
+
+            bool isGoing = true;
+            while (isGoing)
+            {
+                try
+                {
+                    loop(out isGoing);
+                }catch(Exception kuken){
+                    Console.WriteLine(kuken.Message);
+                }
+            }
+        }
+
+        static string PrintMenu()
+        {
+            StringBuilder sb = new StringBuilder();
+            int maxAmount = Enum.GetValues(typeof(RockTypesEnum)).Length;
+            sb.AppendLine($"Please enter the type of rock you have found corresponding to the correct number from 1 to {maxAmount}, or 0 to close the program.");
+            sb.AppendLine(seperator);
+            sb.AppendLine("Press 0 to exit the proram");
+            foreach (RockTypesEnum rockType in Enum.GetValues(typeof(RockTypesEnum)))
+            {
+                sb.AppendLine($"Press {(int)rockType} for {rockType}");
+            }
+            sb.AppendLine($"We have not included any other materials other than these {maxAmount} as they are simply not worth mining at this time.");
+            return sb.ToString();
+        }
+
+        static void loop(out bool keepLooping)
+        {
+            #region user selects rock
+            int userChosenInput = GetIntValueFromConsole(PrintMenu());
+            if (userChosenInput == 0)
+            {
+                Console.WriteLine("Exiting program.");
+                keepLooping = false;
+                return;
+            }
+
+            var rockTypeValues = Enum.GetValues(typeof(RockTypesEnum));
+            if (userChosenInput > rockTypeValues.Length || userChosenInput < 0)
+            {
+                Console.WriteLine("You did not enter a valid value, please enter a number between 1 and 6 for your corresponding material or 0 to close the program");
+                keepLooping = true;
+                return;
+            }
+
+            RockTypesEnum rockType = (RockTypesEnum)userChosenInput;
+            double rockValue = GetRockScuValue(rockType);
+            Console.WriteLine($"You have chosen the material {rockType}, valued at {rockValue} aUEC per mass");
+            #endregion
+
+            double mass = GetDoubleValueFromConsole("Please enter the mass of the rock you have found");
+            double purity = GetDoubleValueFromConsole("Thank you, now please enter the purity of the rock");
+
+            double aUECValue = Math.Round(CalculateAuec(rockType, mass, purity));
+            double scuValue = Math.Round(CalculateScu(mass, purity), 2);
+
+            Console.WriteLine($"Thank you, the estimated SCU and value of this rock is {scuValue} SCU valued at {aUECValue} aUEC.");
+            keepLooping = true;
+        }
+
+        static int GetIntValueFromConsole(string prompt)
+        {
+            Console.WriteLine(prompt);
+            string userInput = Console.ReadLine();
+            return int.Parse(userInput);
+        }
+
+        static double GetDoubleValueFromConsole(string prompt)
+        {
+            Console.WriteLine(prompt);
+            string userInput = Console.ReadLine();
+            double d = double.Parse(userInput);
+            if (d <= 0)
+            {
+                throw new FormatException("Must be greater than 0");
+            }
+            return d;
+        }
+    }
+}
